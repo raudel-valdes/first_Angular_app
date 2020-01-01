d25b8d95fde843404014d1c288caf4865fbca7a7
Raudel Valdes <raudel.valdes2015@gmail.com>
2020-01-01 14:44:39 -0500

    practiced two-way-data-binding and string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = '';
+  serverName = 'TestServer';
+  username = '';
 
   constructor() { 
     setTimeout(() => {
@@ -19,12 +20,20 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
-  onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+  onAddUsername() {
+    this.username = '';
   }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  } 
 
   onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdateUsername(event: any) {
+    this.username = (<HTMLInputElement>event.target).value;
   }
 }
 
